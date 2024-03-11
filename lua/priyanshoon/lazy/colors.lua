84a2de9c58e75adeda90899a7cf07d4f3b25c5c1
priyanshoon <guptapriyanshu973@gmail.com>
2024-03-11 21:29:55 +0530

    nvim theme change to vscode.

--- lua/priyanshoon/lazy/colors.lua
+++ lua/priyanshoon/lazy/colors.lua
@@ -1,18 +1,15 @@
 function ColorMyPencils(color)
-	color = color or "solarized"
+	color = color or "vscode"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
 end
 
 return {
-    {
-        'ishan9299/nvim-solarized-lua',
-        name = 'solarized',
-        config = function()
-            ColorMyPencils('solarized')
-        end
-    }
+    "Mofiqul/vscode.nvim",
+    name = "vscode",
+    config = function()
+        ColorMyPencils("vscode")
+    end
 }
