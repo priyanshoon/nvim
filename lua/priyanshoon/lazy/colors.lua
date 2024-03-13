762366fcd2aeb517019ac6d5dc9a512a06d4ed74
priyanshoon <guptapriyanshu973@gmail.com>
2024-03-13 11:07:18 +0530

    theme changed

--- lua/priyanshoon/lazy/colors.lua
+++ lua/priyanshoon/lazy/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-	color = color or "vscode"
+	color = color or "catppuccin"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -7,9 +7,36 @@ function ColorMyPencils(color)
 end
 
 return {
-    "Mofiqul/vscode.nvim",
-    name = "vscode",
-    config = function()
-        ColorMyPencils("vscode")
-    end
+--     {
+--         "folke/tokyonight.nvim",
+--         name = "tokyonight",
+--         config = function()
+--             local tokyonight = require("tokyonight")
+--             tokyonight.setup({
+--                 style = "storm"
+--             })
+--             ColorMyPencils("tokyonight")
+--         end,
+--     },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        config = function()
+            local catppuccino = require("catppuccin")
+            catppuccino.setup({
+                flavour = "macchiato", -- latte, frappe, macchiato, mocha
+                background = { -- :h background
+                    light = "latte",
+                    dark = "macchiato",
+                },
+                dim_inactive = {
+                    enabled = true, -- dims the background color of inactive window
+                    shade = "dark",
+                    percentage = 0, -- percentage of the shade to apply to the inactive window
+                },
+                show_end_of_buffer = true,
+            })
+            ColorMyPencils("catppuccin")
+        end,
+    }
 }
